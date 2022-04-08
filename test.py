6ebb5c5d1f00cacc2981aaf73c34deb358858d1b
Zeya LT <zeya.zlt@gmail.com>
2022-04-09 00:57:47 +0800

    updated phraseclouds

--- test.py
+++ test.py
@@ -16,12 +16,12 @@ data = {
     "title": {
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/pm_lee.jpg?raw=true",
-            "caption": " <a target=\"_blank\" href=''>credits</a>",
-            "credit": "Prime Minister's Office"
+            # "caption": " <a target=\"_blank\" href=''>credits</a>",
+            "credit": "Source: Prime Minister's Office"
         },
         "text": {
             "headline": "Singapore's<br>COVID-19 Journey",
-            "text": "<p>Through key phrases from PM Lee Hsien Loong's speeches.</p>"
+            "text": "Since COVID-19 struck Singapore in January 2020,<br>PM Lee has addressed the nation several times. Each<br>speech reflected the state of the pandemic as well as<br>measures taken by the Singapore Government. This<br>timeline summarises the journey using key phrases from<br>PM Lee's speeches."
         }
     },
 
@@ -31,22 +31,24 @@ data = {
         # Slide 1
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/phrasecloud_2020-02-08.png?raw=true",
-            "caption": "How to Use TimelineJS (<a target=\"_blank\" href='https://timeline.knightlab.com/'>credits</a>)"
+            "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/PM-Lee-Hsien-Loong-on-the-Novel-Coronavirus-nCoV-Situation-in-Singapore-on-8-February-2020'>Link to speech</a>)"
         },
         "start_date": {
             "year": "2020",
             "month": "2",
-            "day": "8"
+            "day": "8",
+            # "display_date": "HH-MM-YYYY"
         },
         "text": {
-            "headline": "ABCD"
+            "headline": "The Beginning", 
+            "text": " "
         }
         },
         {
         # Slide 2
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/phrasecloud_2020-03-12.png?raw=true",
-            "caption": "Streamlit Components (<a target=\"_blank\" href='https://streamlit.io/'>credits</a>)"
+            "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/PM-Lee-remarks-COVID-19-Outbreak-12-Mar-2020'>Link to speech</a>)"
         },
         "start_date": {
             "year": "2020",
@@ -54,15 +56,15 @@ data = {
             "day":"12"
         },
         "text": {
-            "headline": "Streamlit Components<br>version 0.63.0",
-            "text": "Streamlit lets you turn data scripts into sharable web apps in minutes, not weeks. It's all Python, open-source, and free! And once you've created an app you can use our free sharing platform to deploy, manage, and share your app with the world."
+            # "headline": "Streamlit Components<br>version 0.63.0",
+            # "text": "Streamlit lets you turn data scripts into sharable web apps in minutes, not weeks. It's all Python, open-source, and free! And once you've created an app you can use our free sharing platform to deploy, manage, and share your app with the world."
         }
         },
         {
         # Slide 3
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/phrasecloud_2020-04-03.png?raw=true",
-            "caption": "github/innerdoc (<a target=\"_blank\" href='https://www.github.com/innerdoc/'>credits</a>)"
+            "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/PM-Lee-Hsien-Loong-on-the-COVID19-situation-in-Singapore-on-3-April-2020'>Link to speech</a>)"
         },
         "start_date": {
             "year": "2020",
@@ -70,15 +72,15 @@ data = {
             "day": "3"
         },
         "text": {
-            "headline": "Streamlit TimelineJS component",
-            "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            # "headline": "Streamlit TimelineJS component",
+            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
         }
         },
         {
         # Slide 4
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/phrasecloud_2020-04-10.png?raw=true",
-            "caption": "github/innerdoc (<a target=\"_blank\" href='https://www.github.com/innerdoc/'>credits</a>)"
+            "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/PM-Lee-Hsien-Loong-on-the-COVID-19-situation-in-Singapore-on-10-April-2020'>Link to speech</a>)"
         },
         "start_date": {
             "year": "2020",
@@ -86,15 +88,15 @@ data = {
             "day": "10"
         },
         "text": {
-            "headline": "Streamlit TimelineJS component",
-            "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            # "headline": "Streamlit TimelineJS component",
+            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
         }
         },
         {
         # Slide 5
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/phrasecloud_2020-04-21.png?raw=true",
-            "caption": "github/innerdoc (<a target=\"_blank\" href='https://www.github.com/innerdoc/'>credits</a>)"
+            "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/PM-Lee-Hsien-Loong-address-COVID-19-21-Apr'>Link to speech</a>)"
         },
         "start_date": {
             "year": "2020",
@@ -102,31 +104,35 @@ data = {
             "day": "21"
         },
         "text": {
-            "headline": "Streamlit TimelineJS component",
-            "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            # "headline": "Streamlit TimelineJS component",
+            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
         }
         },
         {
         # Slide 6
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/phrasecloud_2020-06-07.png?raw=true",
-            "caption": "github/innerdoc (<a target=\"_blank\" href='https://www.github.com/innerdoc/'>credits</a>)"
+            "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/National-Broadcast-PM-Lee-Hsien-Loong-COVID-19'>Link to speech</a>)"
         },
         "start_date": {
             "year": "2020",
             "month":"6",
             "day": "7"
         },
+        # "media": {
+        #     "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/after_circuit_breaker.jpg?raw=true",
+        #     "caption": "from ST, after CB"
+        # },
         "text": {
-            "headline": "Streamlit TimelineJS component",
-            "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            # "headline": "Streamlit TimelineJS component",
+            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
         }
         },
         {
         # Slide 7
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/phrasecloud_2020-12-14.png?raw=true",
-            "caption": "github/innerdoc (<a target=\"_blank\" href='https://www.github.com/innerdoc/'>credits</a>)"
+            "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/PM-Lee-Hsien-Loong-on-the-COVID-19-situation-in-Singapore-on-14-December-2020'>Link to speech</a>)"
         },
         "start_date": {
             "year": "2020",
@@ -134,15 +140,15 @@ data = {
             "day": "14"
         },
         "text": {
-            "headline": "Streamlit TimelineJS component",
-            "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            # "headline": "Streamlit TimelineJS component",
+            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
         }
         },
         {
         # Slide 8
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/phrasecloud_2021-05-31.png?raw=true",
-            "caption": "github/innerdoc (<a target=\"_blank\" href='https://www.github.com/innerdoc/'>credits</a>)"
+            "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/PM-Lee-Hsien-Loong-COVID-19-New-Normal-31-May-2021'>Link to speech</a>)"
         },
         "start_date": {
             "year": "2021",
@@ -150,15 +156,15 @@ data = {
             "day": "31"
         },
         "text": {
-            "headline": "Streamlit TimelineJS component",
-            "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            "headline": "The New Normal",
+            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
         }
         },
         {
         # Slide 9
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/phrasecloud_2021-10-09.png?raw=true",
-            "caption": "github/innerdoc (<a target=\"_blank\" href='https://www.github.com/innerdoc/'>credits</a>)"
+            "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/Update-on-the-COVID-19-Situation-In-Singapore-by-Prime-Minister-Lee-Hsien-Loong-on-9-October'>Link to speech</a>)"
         },
         "start_date": {
             "year": "2021",
@@ -174,7 +180,7 @@ data = {
         # Slide 10
         "media": {
             "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/phrasecloud_2022-03-24.png?raw=true",
-            "caption": "github/innerdoc (<a target=\"_blank\" href='https://www.github.com/innerdoc/'>credits</a>)"
+            "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/PM-Lee-Hsien-Loong-on-COVID-19-A-New-Phase-on-24-Mar-2022'>Link to speech</a>)"
         },
         "start_date": {
             "year": "2022",
@@ -182,8 +188,8 @@ data = {
             "day": "24"
         },
         "text": {
-            "headline": "Streamlit TimelineJS component",
-            "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            # "headline": "Streamlit TimelineJS component",
+            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
         }
         },
     ]
