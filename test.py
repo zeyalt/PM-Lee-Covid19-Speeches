829038e537933e3589ac913090abf71d67988d0e
Zeya LT <zeya.zlt@gmail.com>
2022-04-09 01:50:10 +0800

    further updates

--- test.py
+++ test.py
@@ -56,8 +56,8 @@ data = {
             "day":"12"
         },
         "text": {
-            # "headline": "Streamlit Components<br>version 0.63.0",
-            # "text": "Streamlit lets you turn data scripts into sharable web apps in minutes, not weeks. It's all Python, open-source, and free! And once you've created an app you can use our free sharing platform to deploy, manage, and share your app with the world."
+            "headline": "Staying Resilient",
+            "text": '<i>"Singapore’s response has received international accolades. Underlying this is the social and psychological resilience of our people. What makes Singapore different from other countries is that we have confidence in each other, we feel that we are all in this together, and we do not leave anyone behind. This is SG United, we are SG United."'
         }
         },
         {
@@ -72,8 +72,8 @@ data = {
             "day": "3"
         },
         "text": {
-            # "headline": "Streamlit TimelineJS component",
-            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            "headline": "Breaking The Circuit",
+            "text": '<i>"We have decided that instead of tightening incrementally over the next few weeks, we should make a decisive move now to pre-empt escalating infections. We will therefore impose significantly stricter measures. This is like a circuit breaker.</i>"'
         }
         },
         {
@@ -88,8 +88,12 @@ data = {
             "day": "10"
         },
         "text": {
-            # "headline": "Streamlit TimelineJS component",
-            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            "headline": "Outbreak in Foreign Worker Dormitories",
+            "text": '<i>"The next few weeks will be tough. I will speak to you like this from time to time. So that you know what the real situation is, what we are thinking, what you can expect and how you can play your part to fight this virus. The situation will get worse before it gets better, but we have to get through this, before the sun comes out and shines on us again."</i>',
+            # "media": {
+            #     "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/dormitory.jpg?raw=true",
+            #     "credit": "Source: Prime Minister's Office Singapore (<a target=\"_blank\" href='https://www.pmo.gov.sg/Newsroom/PM-Lee-Hsien-Loong-on-the-COVID-19-situation-in-Singapore-on-10-April-2020'>Link to speech</a>)"
+            # }
         }
         },
         {
@@ -104,8 +108,8 @@ data = {
             "day": "21"
         },
         "text": {
-            # "headline": "Streamlit TimelineJS component",
-            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            "headline": "Extending The Circuit Breaker",
+            "text": '<i>"To our migrant workers, let me emphasise again: we will care for you, just like we care for Singaporeans. We thank you for your cooperation during this difficult period. We will look after your health, your welfare and your livelihood. We will work with your employers to make sure that you get paid, and you can send money home. And we will help you stay in touch with friends and family."</i>'
         }
         },
         {
@@ -119,13 +123,9 @@ data = {
             "month":"6",
             "day": "7"
         },
-        # "media": {
-        #     "url": "https://github.com/zeyalt/PM-Lee-Covid19-Speeches/blob/master/Images/after_circuit_breaker.jpg?raw=true",
-        #     "caption": "from ST, after CB"
-        # },
         "text": {
-            # "headline": "Streamlit TimelineJS component",
-            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            "headline": "Overcoming Crisis<br>of a Generation",
+            "text": '<i>"For our plans to succeed, for our hopes and dreams to come true, we need one final ingredient: the unity and resilience of our people. Once in a while, nations and peoples are severely tested, as we are now. Some buckle under pressure and emerge from crisis diminished. Others grow more determined as they face fearful odds, discover reserves of strength in themselves, and emerge from crisis transfigured, renewed. And that has been our Singapore story: in crises, we have never failed to wrest opportunity from danger."</i>'
         }
         },
         {
@@ -140,8 +140,8 @@ data = {
             "day": "14"
         },
         "text": {
-            # "headline": "Streamlit TimelineJS component",
-            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            "headline": "Easing Up Cautiously",
+            "text": '<i>"Now, our defences against COVID-19 are much stronger. We have steadily built up our testing capacities and procedures. We introduced Rostered Routine Testing of higher risk groups. We started using Antigen Rapid Tests, to resume larger gatherings and events safely. We also beefed up our contact tracing capabilities for example, expanding our SafeEntry and TraceTogether programmes, and distributing TraceTogether tokens."</i>'
         }
         },
         {
@@ -156,8 +156,8 @@ data = {
             "day": "31"
         },
         "text": {
-            "headline": "The New Normal",
-            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            "headline": "Test, Trace and Vaccinate",
+            "text": '<i>"We are now dealing with the B.1.617.2 variant, which was first detected in India and is now in over 50 countries. More variants will inevitably emerge, and we will have to deal with them too. What does a more infectious virus mean for our fight against COVID-19? It implies that we must continually adjust our strategies, and raise our game to keep COVID-19 under control. Specifically, there are three things that we have to do more of, and do faster, testing, contact tracing, and vaccination."</i>'
         }
         },
         {
@@ -172,8 +172,8 @@ data = {
             "day": "9"
         },
         "text": {
-            "headline": "Streamlit TimelineJS component",
-            "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            "headline": "Living with COVID-19",
+            "text": '<i>"That is why we are shifting to relying heavily on Home Recovery. It will be the norm for COVID-19 cases. You can get well in a familiar home setting, without the stress and bother of admitting yourself into a care facility. If most of us can recover at home, it will greatly ease the strain on our hospitals, doctors and nurses. Next, since COVID-19 has become a manageable disease, we should now drastically simplify our health protocols. No more complicated flow charts. People must be clear what to do if they test positive, or if they come into contact with someone who is infected. "</i>'
         }
         },
         {
@@ -188,8 +188,8 @@ data = {
             "day": "24"
         },
         "text": {
-            # "headline": "Streamlit TimelineJS component",
-            # "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it."
+            "headline": "A Turning Point",
+            "text": '<i>"Taking all things into consideration, we believe that we are now ready to take a decisive step forward towards living with COVID-19. Resume more normal lives, enjoy larger gatherings of family and friends, go outdoors without masks, or reunite with loved ones abroad. But do not throw all caution to the wind. Each one of us must still play our part."</i>'
         }
         },
     ]
